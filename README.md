e7988ecfc0b260e0debeceb1bdff7129bdc9aa97
Abigail Watson <awatson1978@gmail.com>
2015-10-18 16:53:41 -0700

    update readme; namespace

---
+++ README.md
@@ -0,0 +1,15 @@
+## photonic:no-select-everything
+
+Disable user-select by default. Need to opt-in.
+
+
+### Installation
+
+````js
+meteor add photonic:no-select-everything
+````
+
+
+### Licensing
+
+MIT License.  Use as you will.
--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
-  name: 'clinical:no-select-everything',
+  name: 'photonic:no-select-everything',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'photonic:no-select-everything',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/awatson1978/no-select-everything',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
